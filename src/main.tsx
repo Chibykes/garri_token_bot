c5f4600eb22edea434894fcdb23ade50641c94be
Chukwubuikem Chiabuotu <chibykes99@gmail.com>
2024-08-12 12:21:50 +0100

    Log platform

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './index.css'
 import WebApp from '@twa-dev/sdk'
 
 WebApp.ready();
+console.log(WebApp.platform);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
